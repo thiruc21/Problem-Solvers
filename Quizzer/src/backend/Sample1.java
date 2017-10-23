37f666c7249b3626b4dd12bc9537b8967a4ce9b5
Vijanthan <vijanthan.thiruchelvarajah@mail.utoronto.ca>
2017-10-22 21:22:32 -0400

    Added initial baseline of our project. Essentially, an empty project template in which we will branch off and work in feature branches.

---
+++ Quizzer/src/backend/Sample1.java
@@ -0,0 +1,14 @@
+package backend;
+
+public class Sample1 {
+
+	public Sample1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
