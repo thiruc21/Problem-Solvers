5d747460cab41e049d50a281e69bda09d9c75e7d
Chengli Yang <chengliyang417@hotmail.com>
2017-10-25 01:45:29 -0500

    Committed starting GUI code for adding questions

---
+++ Quizzer/src/frontend/Add_questions_gui.java
@@ -0,0 +1,43 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Add_questions_gui {
+
+  private JFrame frame;
+
+  /**
+   * Launch the application.
+   */
+  public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        try {
+          Add_questions_gui window = new Add_questions_gui();
+          window.frame.setVisible(true);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
+
+  /**
+   * Create the application.
+   */
+  public Add_questions_gui() {
+    initialize();
+  }
+
+  /**
+   * Initialize the contents of the frame.
+   */
+  private void initialize() {
+    frame = new JFrame();
+    frame.setBounds(100, 100, 450, 300);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+
+}
