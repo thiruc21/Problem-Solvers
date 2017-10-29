49946385d40c9e8b586c0e49b6101deefbcd36a2
Chengli Yang <chengliyang417@hotmail.com>
2017-10-28 23:09:25 -0500

    Created GUI for adding questions based on textbox implmentation

--- Quizzer/src/frontend/Add_questions_gui.java
+++ Quizzer/src/frontend/Add_questions_gui.java
@@ -3,10 +3,19 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.GridLayout;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JCheckBox;
+import java.awt.BorderLayout;
+import javax.swing.JList;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class Add_questions_gui {
 
   private JFrame frame;
+  private JTextField textField;
 
   /**
    * Launch the application.
@@ -36,8 +45,37 @@ public class Add_questions_gui {
    */
   private void initialize() {
     frame = new JFrame();
-    frame.setBounds(100, 100, 450, 300);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().setLayout(null);
+    
+    @SuppressWarnings("rawtypes")
+    JList list = new JList();
+    list.setBounds(40, 11, 356, 164);
+    frame.getContentPane().add(list);
+    
+    String[] q = new String[3];
+    q[0] = "a";
+    q[1] = "b";
+    q[2] = "c";
+    showQuestionsList(list, q);
+    
+    textField = new JTextField();
+    textField.setBounds(40, 230, 86, 20);
+    frame.getContentPane().add(textField);
+    textField.setColumns(10);
+    
+    JLabel lblEnter = new JLabel("Enter the numbers of the questions you want to insert:");
+    lblEnter.setBounds(40, 205, 356, 14);
+    frame.getContentPane().add(lblEnter);
   }
-
+  
+  @SuppressWarnings("unchecked")
+  private void showQuestionsList(@SuppressWarnings("rawtypes") JList list, String questions[]) {
+    @SuppressWarnings("rawtypes")
+    DefaultListModel dlm = new DefaultListModel();
+    for(int i = 1; i < questions.length+1; i++) {
+      dlm.addElement(i + ". " + questions[i-1]);
+    }
+    list.setModel(dlm);
+  }
 }
+
