9089c62a209be9ef486a3f095021ae5ca1d1b0fc
Chengli Yang <chengliyang417@hotmail.com>
2017-11-18 01:14:02 -0600

    Made changes to the ValidationTest.java

--- Quizzer/src/ValidationTest.java
+++ Quizzer/src/ValidationTest.java
@@ -1,7 +1,11 @@
 import java.awt.Component;
+import java.awt.event.MouseEvent;
 
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JRadioButton;
 
 import application.Quizzer;
 
@@ -10,54 +14,53 @@ public class ValidationTest {
 		Quizzer q = new Quizzer();
 		Component[] comps = q.frame.getContentPane().getComponents();
 		
-		int i = 0;
 		int p = 0;
 		int n = 0;
-	
-		for(Component comp: comps) {		
+		String text = "";
+		String component = "";
+		
+		for(Component comp: comps) {
+			if (comp instanceof JButton) {
+				text = ((JButton) comp).getText();				
+				component = "JButton";
+			} else if (comp instanceof JLabel) {
+				text = ((JLabel) comp).getText();	
+				component = "JLabel";
+			} else if (comp instanceof JList) {
+				component = "JList";
+			} else if (comp instanceof JRadioButton) {
+				text = ((JRadioButton) comp).getText();	
+				component = "JRadioButton";
+			} 
 			if (comp.isShowing()) {
-				System.out.println("+Component " + i + " is showing on the screen.");
-				System.out.println(comp.getName());
+				System.out.println("+" + text + " " + component + " is showing on the screen.");
 				p++;
 			} else {
-				System.out.println("-Component " + i + " is not showing on the screen.");
-				System.out.println(comp.getName());
+				System.out.println("-" + text + " " + component + " is not showing on the screen.");
 				n++;
 			}
 			if (comp.isValid()) {
-				System.out.println("+Component " + i + " is valid.");
-				System.out.println(comp.getName());
+				System.out.println("+" + text + " " + component + " is valid.");
 				p++;
 			} else {
-				System.out.println("-Component " + i + " is not valid.");
-				System.out.println(comp.getName());
+				System.out.println("-" + text + " " + component + " is not valid.");
 				n++;
 			}
 			if (comp.isVisible()) {
-				System.out.println("+Component " + i + " is visible.");
-				System.out.println(comp.getName());
+				System.out.println("+" + text + " " + component + " is visible.");
 				p++;
 			} else {
-				System.out.println("-Component " + i + " is not visible.");
-				System.out.println(comp.getName());
+				System.out.println("-" + text + " " + component + " is not visible.");
 				n++;
 			}
 			if (comp.isEnabled()) {
-				System.out.println("+Component " + i + " is enabled.");
-				System.out.println(comp.getName());
+				System.out.println("+" + text + " " + component + " is enabled.");
 				p++;
 			} else {
-				System.out.println("-Component " + i + " is not enabled.");
-				System.out.println(comp.getName());
+				System.out.println("-" + text + " " + component + " is not enabled.");
 				n++;
 			}
-			
-			if (comp instanceof JButton) {
-				System.out.println("WOW");
-			} else if (comp instanceof JList) {
-				
-			} 
-			i++;
+			System.out.println("");
 		}
 		System.out.println("Positive test results: " + p);
 		System.out.println("Negative test results: " + n);
